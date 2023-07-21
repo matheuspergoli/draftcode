c162c6c6c4d65c533cb95e72fc835d0abc2fd3dd
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-07-21 20:57:06 -0300

    fix: resolve exceeded limit for serverless functions

--- src/externals/libs/redis.ts
+++ src/externals/libs/redis.ts
@@ -1,6 +1,23 @@
 import { Redis } from 'ioredis'
-import { MockRedis } from '@mocks/MockRedis'
 
 const isDEV = process.env.NODE_ENV === 'development'
+
+const MockRedis = () => {
+	return {
+		get: () => {
+			return null
+		},
+
+		set: () => {
+			return new Promise<void>((resolve) => {
+				resolve()
+			})
+		},
+
+		del: () => {
+			return null
+		}
+	} as unknown as Redis
+}
 
 export const redis = isDEV ? MockRedis() : new Redis(process.env.REDIS_URL as string)

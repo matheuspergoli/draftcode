0e29eca8fa917d19ce3b6ecb6ccfbe4f28582ab3
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-07-21 23:36:27 -0300

    chore: update configs

--- src/presentation/components/Dashboard/ChallengeList/ChallengeListCard/ChallengeListCard.tsx
+++ src/presentation/components/Dashboard/ChallengeList/ChallengeListCard/ChallengeListCard.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@components/ui/button'
 import Link from 'next/link'
 import { DeleteButton } from './DeleteButton'
+import { Button } from '@components/ui/button'
 
 interface ChallengeListCardProps {
 	id: string

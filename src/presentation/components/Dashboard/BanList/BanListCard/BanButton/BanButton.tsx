39e95bf203a5e9bf976ef83814e8cc57bbf6000d
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-08-11 07:04:01 -0300

    feat: add loading for dashboard

--- src/presentation/components/Dashboard/BanList/BanListCard/BanButton/BanButton.tsx
+++ src/presentation/components/Dashboard/BanList/BanListCard/BanButton/BanButton.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
 import { Button } from '@components/ui/button'
+import { ReloadIcon } from '@radix-ui/react-icons'
 import { useToast } from '@components/ui/use-toast'
 
 import {
@@ -35,9 +36,11 @@ export const BanButton: React.FC<BanButtonProps> = ({ id, isBanned }) => {
 	const { toast } = useToast()
 	const { update } = useSession()
 	const isUserBanned = isBanned ? 'Banir' : 'Desbanir'
+	const [loading, setLoading] = React.useState(false)
 	const [banned, setBanned] = React.useState(isUserBanned)
 
 	const handleUpdate = async () => {
+		setLoading(true)
 		const response = await fetch(`/api/users/ban/${id}`, {
 			method: 'PUT',
 			body: JSON.stringify({ banned: banned }),
@@ -53,7 +56,9 @@ export const BanButton: React.FC<BanButtonProps> = ({ id, isBanned }) => {
 			})
 
 			update()
+			setLoading(false)
 		} else {
+			setLoading(false)
 			toast({
 				title: 'Erro ao atualizar usuário',
 				description: 'Tente novamente'
@@ -64,30 +69,48 @@ export const BanButton: React.FC<BanButtonProps> = ({ id, isBanned }) => {
 	}
 
 	return (
-		<Dialog>
-			<DialogTrigger asChild>
-				<Button>Editar</Button>
-			</DialogTrigger>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>Banimento</DialogTitle>
-					<DialogDescription>Escolha a ação que deseja realizar</DialogDescription>
-				</DialogHeader>
-				<Select defaultValue={isUserBanned} onValueChange={(banned) => setBanned(banned)}>
-					<SelectTrigger className='w-[180px]'>
-						<SelectValue placeholder='Banimentos' />
-					</SelectTrigger>
-					<SelectContent>
-						<SelectItem value='Desbanir'>Desbanir</SelectItem>
-						<SelectItem value='Banir'>Banir</SelectItem>
-					</SelectContent>
-				</Select>
-				<DialogFooter>
-					<DialogClose asChild>
-						<Button onClick={() => handleUpdate()}>Confirmar</Button>
-					</DialogClose>
-				</DialogFooter>
-			</DialogContent>
-		</Dialog>
+		<>
+			{loading && (
+				<div className='fixed inset-0 z-50 flex items-center justify-center'>
+					<div className='absolute inset-0 bg-background opacity-80' />
+					<div className='relative'>
+						<div className='rounded-lg border border-border bg-[#1F1F1F] p-10 text-foreground'>
+							<h1 className='text-2xl font-bold'>
+								Atualizando Usuário
+								<ReloadIcon className='ml-2 inline-block h-6 w-6 animate-spin' />
+							</h1>
+							<p className='mt-5'>Aguarde enquanto o usuário é atualizado.</p>
+						</div>
+					</div>
+				</div>
+			)}
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button>Editar</Button>
+				</DialogTrigger>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Banimento</DialogTitle>
+						<DialogDescription>Escolha a ação que deseja realizar</DialogDescription>
+					</DialogHeader>
+					<Select
+						defaultValue={isUserBanned}
+						onValueChange={(banned) => setBanned(banned)}>
+						<SelectTrigger className='w-[180px]'>
+							<SelectValue placeholder='Banimentos' />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value='Desbanir'>Desbanir</SelectItem>
+							<SelectItem value='Banir'>Banir</SelectItem>
+						</SelectContent>
+					</Select>
+					<DialogFooter>
+						<DialogClose asChild>
+							<Button onClick={() => handleUpdate()}>Confirmar</Button>
+						</DialogClose>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</>
 	)
 }
--- src/presentation/components/Dashboard/UsersList/UsersCard/UpdateButton/UpdateButton.tsx
+++ src/presentation/components/Dashboard/UsersList/UsersCard/UpdateButton/UpdateButton.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
 import { Button } from '@components/ui/button'
+import { ReloadIcon } from '@radix-ui/react-icons'
 import { useToast } from '@components/ui/use-toast'
 
 import {
@@ -32,8 +33,10 @@ export const UpdateButton: React.FC<UpdateButtonProps> = ({ id }) => {
 	const router = useRouter()
 	const { toast } = useToast()
 	const [role, setRole] = React.useState('USER')
+	const [loading, setLoading] = React.useState(false)
 
 	const handleUpdate = async () => {
+		setLoading(true)
 		const response = await fetch(`/api/users/${id}`, {
 			method: 'PUT',
 			body: JSON.stringify({ role }),
@@ -47,7 +50,10 @@ export const UpdateButton: React.FC<UpdateButtonProps> = ({ id }) => {
 				title: 'Usuário atualizado com sucesso',
 				description: 'Seu usuário foi atualizado com sucesso'
 			})
+
+			setLoading(false)
 		} else {
+			setLoading(false)
 			toast({
 				title: 'Erro ao atualizar usuário',
 				description: 'Tente novamente'
@@ -58,31 +64,47 @@ export const UpdateButton: React.FC<UpdateButtonProps> = ({ id }) => {
 	}
 
 	return (
-		<Dialog>
-			<DialogTrigger asChild>
-				<Button>Editar</Button>
-			</DialogTrigger>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>Modificar cargo</DialogTitle>
-					<DialogDescription>Escolha o novo cargo para o usuário</DialogDescription>
-				</DialogHeader>
-				<Select defaultValue='USER' onValueChange={(role) => setRole(role)}>
-					<SelectTrigger className='w-[180px]'>
-						<SelectValue placeholder='Cargos' />
-					</SelectTrigger>
-					<SelectContent>
-						<SelectItem value='USER'>USER</SelectItem>
-						<SelectItem value='ADMIN'>ADMIN</SelectItem>
-						<SelectItem value='SUPERADMIN'>SUPERADMIN</SelectItem>
-					</SelectContent>
-				</Select>
-				<DialogFooter>
-					<DialogClose asChild>
-						<Button onClick={() => handleUpdate()}>Confirmar</Button>
-					</DialogClose>
-				</DialogFooter>
-			</DialogContent>
-		</Dialog>
+		<>
+			{loading && (
+				<div className='fixed inset-0 z-50 flex items-center justify-center'>
+					<div className='absolute inset-0 bg-background opacity-80' />
+					<div className='relative'>
+						<div className='rounded-lg border border-border bg-[#1F1F1F] p-10 text-foreground'>
+							<h1 className='text-2xl font-bold'>
+								Atualizando Usuário
+								<ReloadIcon className='ml-2 inline-block h-6 w-6 animate-spin' />
+							</h1>
+							<p className='mt-5'>Aguarde enquanto o usuário é atualizado.</p>
+						</div>
+					</div>
+				</div>
+			)}
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button>Editar</Button>
+				</DialogTrigger>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Modificar cargo</DialogTitle>
+						<DialogDescription>Escolha o novo cargo para o usuário</DialogDescription>
+					</DialogHeader>
+					<Select defaultValue='USER' onValueChange={(role) => setRole(role)}>
+						<SelectTrigger className='w-[180px]'>
+							<SelectValue placeholder='Cargos' />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value='USER'>USER</SelectItem>
+							<SelectItem value='ADMIN'>ADMIN</SelectItem>
+							<SelectItem value='SUPERADMIN'>SUPERADMIN</SelectItem>
+						</SelectContent>
+					</Select>
+					<DialogFooter>
+						<DialogClose asChild>
+							<Button onClick={() => handleUpdate()}>Confirmar</Button>
+						</DialogClose>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</>
 	)
 }
